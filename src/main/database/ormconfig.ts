7ca650aaf51fefc28ee66e388e0398360ba6cf5a
Paulo Pires <ppiresdev@gmail.com>
2023-01-08 14:31:46 -0300

    ajusta ormconfig para usar sqlite

--- src/main/database/ormconfig.ts
+++ src/main/database/ormconfig.ts
@@ -1,20 +1,33 @@
 import "dotenv/config";
 import { DataSourceOptions } from "typeorm";
 
-const rootDir = process.env.NODE_ENV === "production" ? "dist" : "src";
+let config: DataSourceOptions;
 
-console.log("ROOT DIR: " + rootDir);
-console.log("NODE ENV: " + process.env.NODE_ENV);
-const config: DataSourceOptions = {
-  type: "postgres",
-  url: process.env.DATABASE_URL,
-  synchronize: false,
-  logging: false,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  entities: [rootDir + "/app/shared/database/entities/*"],
-  migrations: [rootDir + "/main/database/migrations/*"],
-};
+if (process.env.NODE_ENV === "test") {
+  config = {
+    type: "sqlite",
+    database: "./test.sqlite",
+    synchronize: false,
+    logging: false,
+    entities: ["src/app/shared/database/entities/*"],
+    migrations: ["src/main/database/migrations/*"],
+  };
+} else {
+  const rootDir = process.env.NODE_ENV === "production" ? "dist" : "src";
+
+  console.log("ROOT DIR: " + rootDir);
+  console.log("NODE ENV: " + process.env.NODE_ENV);
+  config = {
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    synchronize: false,
+    logging: false,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+    entities: [rootDir + "/app/shared/database/entities/*"],
+    migrations: [rootDir + "/main/database/migrations/*"],
+  };
+}
 
 export default config;
